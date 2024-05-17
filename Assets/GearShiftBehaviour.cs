e761270c2a1c95e89599b851d313ba7fba4002c1
Agon <agonthedev@gmail.com>
2024-05-17 14:03:04 -0700

    Unity Project Generated
    Core3D

---
+++ Assets/GearShiftBehaviour.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearShiftBehaviour : MonoBehaviour
+{
+
+    // private bool _held = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        // Detect
+    }
+}
